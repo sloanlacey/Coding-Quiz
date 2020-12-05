0dfe26f1ec61b63bdf28691ae07dd1d4c812c399
sloanlacey <sloanlacey89@gmail.com>
2020-12-04 18:33:11 -0800

    Add comments to highscore JS page.

--- highscores.js
+++ highscores.js
@@ -1,9 +1,11 @@
 const highScoresList = document.getElementById("highScoresList");
+// Same syntax as end page JS
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-// GO OVER MAP METHOD
+// Map method to add <li>'s to a list instead of leaving them as strings--writing HTML inside the JS
 highScoresList.innerHTML = highScores
     .map( score => {
         return `<li class="high-score">${score.name} - ${score.score}</li>`;
 })
     .join("");
+// .join will join all the elements in the array with an empty string--generates a string with all the <li> content
